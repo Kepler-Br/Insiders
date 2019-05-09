75e5325880cfbcc61b651a2f41f96fd45401afef
Barrett Kepler <Kepler-br@yandex.ru>
2019-05-09 18:35:15 +0300

    User auth almost done

--- UserAuthApp/urls.py
+++ UserAuthApp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('invite/<str:invite_slug>', views.RegistrationPage.as_view(), name="register_page"),
     path('request_invite', views.RequestInvitePage.as_view(), name="request_invite_page"),
     path('logout', views.LogoutPage.as_view(), name="logout_page"),
-    path('login', views.LoginPage.as_view(), name="login_page"),
+    path('login', auth_views.LoginView.as_view(template_name="UserAuthApp/login.html"), name="login_page"),
 ]
