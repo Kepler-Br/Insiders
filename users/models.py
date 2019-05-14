e7a8c0730d3074aad28520f97084e522cfd34e2b
Barrett Kepler <kepler-br@yandex.ru>
2019-05-14 22:00:25 +0300

    Sum chungus

--- users/models.py
+++ users/models.py
@@ -17,19 +17,12 @@ class InviteRequests(models.Model):
 
 
 class Profile(models.Model):
-    GENDER_CHOICES = (
-        ('M', 'Male'),
-        ('F', 'Female'),
-        ('U', 'Undefined'),
-        ('H', 'Helisexual'),
-    )
-
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='profile_pictures/default_avatar.png', upload_to="profile_pictures")
     # short_about = models.CharField(max_length=100, blank=True)
     about = models.CharField(max_length=1000, blank=True)
     status = models.CharField(max_length=256, blank=True)
-    gender = models.CharField(max_length=1, choices=GENDER_CHOICES, default='U')
+    gender = models.CharField(max_length=25, blank=False, default='Undefined')
 
     def __str__(self):
         return f"{self.user.username}'s profile"
