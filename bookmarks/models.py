cd2d073f0f440053829d2d897a2df91e3777499e
Barrett Kepler <kepler-br@yandex.ru>
2019-05-15 23:01:21 +0300

    meh

--- bookmarks/models.py
+++ bookmarks/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.shortcuts import reverse
 
 # Create your models here.
 
@@ -10,13 +11,18 @@ class BookmarkFolder(models.Model):
     slug = models.SlugField(max_length=20)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     is_hidden = models.BooleanField(default=False)
+    image = models.ImageField(default='bookmark_folder_avatars/default_avatar.png', upload_to="bookmark_folder_avatars")
 
     def __str__(self):
         return f"{self.title}"
 
+    def get_absolute_url(self):
+        return reverse('bookmark_folder', kwargs={'slug': self.slug})
+
 
 class Bookmark(models.Model):
     body = models.TextField(max_length=4000, db_index=True, blank=False)
+    image = models.ImageField(default='bookmark_avatars/default_avatar.png', upload_to="bookmark_avatars")
     title = models.TextField(max_length=100, db_index=True, blank=False)
     tags = models.TextField(max_length=500, db_index=True, blank=True)
     slug = models.SlugField(max_length=20)
@@ -27,3 +33,8 @@ class Bookmark(models.Model):
 
     def __str__(self):
         return f"{self.title}"
+
+    def get_absolute_url(self):
+        return reverse('bookmark', kwargs={'slug': self.slug})
+
+
