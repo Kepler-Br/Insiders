29b49d8a73ef0fef99b38acd8b4566fdf24afc73
Barrett Kepler <45532267+Kepler-Br@users.noreply.github.com>
2019-05-15 15:02:21 +0300

    Some modifications to bookmark folder system were added

--- bookmarks/urls.py
+++ bookmarks/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('user/<str:user_slug>/bookmarks', views.UserBookmarksFolders.as_view(), name="user_bookmark_folders"),
+    path('bookmarks/create', views.CreateBookmarkFolder.as_view(), name="user_bookmark_folder_create"),
 ]
--- bookmarks/views.py
+++ bookmarks/views.py
@@ -2,7 +2,16 @@ from django.shortcuts import render
 from django.views.generic import View
 from django.contrib.auth.models import User
 from .models import BookmarkFolder
+from .forms import CreateBookmarkFolderForm
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+    View
+)
 
 # Create your views here.
 
@@ -11,5 +20,15 @@ class UserBookmarksFolders(LoginRequiredMixin, View):
     def get(self, request, user_slug):
         user = User.objects.get(username=user_slug)
         bookmark_folders = BookmarkFolder.objects.filter(author=user)
-        context = {"folders": bookmark_folders}
+        context = {"folders": bookmark_folders, "author": user}
         return render(request, "bookmarks/bookmark_folders.html", context=context)
+
+
+class CreateBookmarkFolder(LoginRequiredMixin, CreateView):
+    model = BookmarkFolder
+    form_class = CreateBookmarkFolderForm
+    template_name = 'bookmarks/create_bookmark_folder.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
